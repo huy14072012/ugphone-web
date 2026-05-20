47c0c48d59314605642aa7e5cf32f982b31c4f1c
huy14072012 <ghuy1472012@gmail.com>
2026-05-20 13:01:56 +0700

    Add files via upload

---
+++ kho-acc.js
@@ -0,0 +1,8 @@
+// KHO CHỨA TÀI KHOẢN XOAY TUA THEO THỜI GIAN
+const listAcc = [
+    // Tài khoản số 1 (Đoạn mã Token của bạn)
+    `{"ugPhoneLang":"en","ugBrowserId":"d57975d4e5bf38fe996fa53b79f88b85","UGPHONE-ID":"27ee405c-676e-0f0d-5165-55165c0bcd9f","hadAgreePolicy":"true","UGPHONE-Token":"083d6e453df06685a01cdef074af7fee20260519200114","UGPHONE-MQTT":"{\\"client_id\\":18467474,\\"login_id\\":\\"27ee405c-676e-0f0d-5165-55165c0bcd9f\\",\\"access_token\\":\\"083d6e453df06685a01cdef074af7fee20260519200114\\",\\"access_key\\":\\"11696dc4d44c2b051cdd8384e9b91521\\",\\"access_secret\\":\\"11696dc4dbd4018e1a830dbd44c2b051\\",\\"mqtt_url\\":\\"tcp://mqtt.ugphone.com:20001\\",\\"mqtt_ssl_url\\":\\"ssl://mqtt.ugphone.com:20003\\",\\"mqtt_ws_url\\":\\"ws://mqtt.ugphone.com:20002/mqtt\\",\\"mqtt_wss_url\\":\\"wss://mqtt.ugphone.com:8183/mqtt\\",\\"mqtt_topic\\":\\"game-toc/online/mqtt_client_18467474/2567a5ec9705eb7ac2c984033e06189d\\",\\"mqtt_client_id\\":\\"mqtt_client_18467474_web\\",\\"mqtt_username\\":\\"mqtt_client_18467474\\",\\"mqtt_password\\":\\"73b9688093c52897fb394ad92074844a\\",\\"ws_url\\":\\"wss://www.ugphone.com:2351\\",\\"first_login\\":1,\\"is_visitor\\":0}"}`,
+    
+    // Tài khoản số 2 (Dán thêm acc mới vào giữa dấu backtick nếu có)
+    `{"UGPHONE-ID":"ACC_SO_2_CỦA_BẠN"}`
+];
